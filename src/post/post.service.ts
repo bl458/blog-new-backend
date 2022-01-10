4cda0d12c0a73b387aeefc208d3852783c5915f0
Byungchan Lim <bl458@cornell.edu>
2022-01-10 19:33:38 +0900

    FEAT implement post fetching per page using caching

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -9,10 +9,24 @@ import { PostDTO } from 'src/dto/post.dto';
 
 @Injectable()
 export class PostService {
+  static CACHE_DURATION = 86400000; //1 day
+  static PAGE_SIZE = 5;
+
   constructor(private conn: DBConnService) {}
 
   async doGetPostPage(pageNo: number): Promise<Post[]> {
-    return [];
+    return this.conn.getConn().transaction(async (mgr) => {
+      const posts = await mgr
+        .createQueryBuilder(Post, 'post')
+        .select(['post.id', 'post.title', 'post.titleSub', 'post.content'])
+        .orderBy('post.createdAt', 'DESC')
+        .cache(PostService.CACHE_DURATION)
+        .getMany();
+
+      const pageStart = pageNo * PostService.PAGE_SIZE;
+      const pageEnd = (pageNo + 1) * PostService.PAGE_SIZE;
+      return posts.slice(pageStart, pageEnd);
+    });
   }
 
   async doEditPost(postDTO: PostDTO): Promise<void> {
