cdcd782cceffc52c78165074cd0b652a7dc3eb55
Byungchan Lim <bl458@cornell.edu>
2022-01-11 12:16:22 +0900

    FIX modules

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 
+import { DBModule } from 'src/db/db.module';
+
 import { PostController } from './post.controller';
 
 import { PostService } from './post.service';
@@ -7,6 +9,7 @@ import { PostService } from './post.service';
 @Module({
   controllers: [PostController],
   providers: [PostService],
+  imports: [DBModule],
   exports: [PostService],
 })
 export class PostModule {}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 
 import { AuthModule } from 'src/auth/auth.module';
 import { DBModule } from 'src/db/db.module';
+import { PostModule } from 'src/post/post.module';
 
 import { UserController } from './user.controller';
 import { UserSessionController } from './user.session.controller';
@@ -9,11 +10,10 @@ import { UserPostController } from './user.post.controller';
 
 import { UserService } from './user.service';
 import { UserSessionService } from './user.session.service';
-import { PostService } from 'src/post/post.service';
 
 @Module({
-  imports: [AuthModule, DBModule, PostService],
   controllers: [UserController, UserSessionController, UserPostController],
   providers: [UserService, UserSessionService],
+  imports: [AuthModule, DBModule, PostModule],
 })
 export class UserModule {}
