2990651f513de481df5cc1c8a466feb3ddf1df70
Byungchan Lim <bl458@cornell.edu>
2022-01-11 13:24:12 +0900

    CHORE update migration

--- src/db/migration/1641874988730-init.ts
+++ src/db/migration/1641874988730-init.ts
@@ -1,20 +1,20 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class init1641871334886 implements MigrationInterface {
-  name = 'init1641871334886';
+export class init1641874988730 implements MigrationInterface {
+  name = 'init1641874988730';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       `CREATE TABLE \`tag\` (\`id\` varchar(36) NOT NULL, \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`name\` varchar(255) NOT NULL, \`postId\` varchar(36) NULL, UNIQUE INDEX \`IDX_6a9775008add570dc3e5a0bab7\` (\`name\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
     );
     await queryRunner.query(
-      `CREATE TABLE \`user\` (\`id\` varchar(36) NOT NULL, \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`email\` varchar(255) NOT NULL, \`pw\` varchar(255) NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
+      `CREATE TABLE \`user\` (\`id\` varchar(36) NOT NULL, \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`email\` varchar(255) NOT NULL, \`pw\` varchar(255) NOT NULL, INDEX \`IDX_e12875dfb3b1d92d7d7c5377e2\` (\`email\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
     );
     await queryRunner.query(
-      `CREATE TABLE \`post\` (\`id\` varchar(36) NOT NULL, \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`title\` varchar(255) NOT NULL, \`titleSub\` varchar(255) NOT NULL, \`content\` varchar(255) NOT NULL, \`userId\` varchar(36) NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
+      `CREATE TABLE \`post\` (\`id\` varchar(36) NOT NULL, \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`title\` varchar(255) NOT NULL, \`titleSub\` varchar(255) NOT NULL, \`content\` varchar(255) NOT NULL, \`userId\` varchar(36) NULL, INDEX \`IDX_fb91bea2d37140a877b775e6b2\` (\`createdAt\`), INDEX \`IDX_5c1cf55c308037b5aca1038a13\` (\`userId\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
     );
     await queryRunner.query(
-      `CREATE TABLE \`user_session\` (\`id\` varchar(36) NOT NULL, \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`token\` varchar(256) NOT NULL, \`userId\` varchar(36) NULL, UNIQUE INDEX \`REL_b5eb7aa08382591e7c2d1244fe\` (\`userId\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
+      `CREATE TABLE \`user_session\` (\`id\` varchar(36) NOT NULL, \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`token\` varchar(256) NOT NULL, \`userId\` varchar(36) NULL, INDEX \`IDX_b330b1d48bb9384e7f4b8efbc0\` (\`token\`), INDEX \`IDX_b5eb7aa08382591e7c2d1244fe\` (\`userId\`), INDEX \`IDX_c35ccb7d788fd7de3414465ddc\` (\`token\`, \`createdAt\`), UNIQUE INDEX \`REL_b5eb7aa08382591e7c2d1244fe\` (\`userId\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
     );
     await queryRunner.query(
       `ALTER TABLE \`tag\` ADD CONSTRAINT \`FK_7435e891c35f2687d7969490476\` FOREIGN KEY (\`postId\`) REFERENCES \`post\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`,
@@ -44,8 +44,26 @@ export class init1641871334886 implements MigrationInterface {
     await queryRunner.query(
       `DROP INDEX \`REL_b5eb7aa08382591e7c2d1244fe\` ON \`user_session\``,
     );
+    await queryRunner.query(
+      `DROP INDEX \`IDX_c35ccb7d788fd7de3414465ddc\` ON \`user_session\``,
+    );
+    await queryRunner.query(
+      `DROP INDEX \`IDX_b5eb7aa08382591e7c2d1244fe\` ON \`user_session\``,
+    );
+    await queryRunner.query(
+      `DROP INDEX \`IDX_b330b1d48bb9384e7f4b8efbc0\` ON \`user_session\``,
+    );
     await queryRunner.query(`DROP TABLE \`user_session\``);
+    await queryRunner.query(
+      `DROP INDEX \`IDX_5c1cf55c308037b5aca1038a13\` ON \`post\``,
+    );
+    await queryRunner.query(
+      `DROP INDEX \`IDX_fb91bea2d37140a877b775e6b2\` ON \`post\``,
+    );
     await queryRunner.query(`DROP TABLE \`post\``);
+    await queryRunner.query(
+      `DROP INDEX \`IDX_e12875dfb3b1d92d7d7c5377e2\` ON \`user\``,
+    );
     await queryRunner.query(`DROP TABLE \`user\``);
     await queryRunner.query(
       `DROP INDEX \`IDX_6a9775008add570dc3e5a0bab7\` ON \`tag\``,
