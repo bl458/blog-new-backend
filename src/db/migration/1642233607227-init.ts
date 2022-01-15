87c8f40d656de47beee78bf3bed09a48e8fde783
Byungchan Lim <bl458@cornell.edu>
2022-01-15 17:00:40 +0900

    CHORE update migration

--- src/db/migration/1642233607227-init.ts
+++ src/db/migration/1642233607227-init.ts
@@ -1,13 +1,13 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class init1642101645315 implements MigrationInterface {
-    name = 'init1642101645315'
+export class init1642233607227 implements MigrationInterface {
+    name = 'init1642233607227'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE \`tag\` (\`id\` varchar(36) NOT NULL, \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`name\` varchar(255) NOT NULL, \`postId\` varchar(36) NULL, UNIQUE INDEX \`IDX_6a9775008add570dc3e5a0bab7\` (\`name\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`CREATE TABLE \`user_session\` (\`id\` varchar(36) NOT NULL, \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`token\` varchar(256) NOT NULL, \`userId\` varchar(36) NULL, INDEX \`IDX_b5eb7aa08382591e7c2d1244fe\` (\`userId\`), INDEX \`IDX_c35ccb7d788fd7de3414465ddc\` (\`token\`, \`createdAt\`), UNIQUE INDEX \`IDX_b330b1d48bb9384e7f4b8efbc0\` (\`token\`), UNIQUE INDEX \`REL_b5eb7aa08382591e7c2d1244fe\` (\`userId\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`CREATE TABLE \`user\` (\`id\` varchar(36) NOT NULL, \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`email\` varchar(255) NOT NULL, \`pw\` varchar(255) NOT NULL, UNIQUE INDEX \`IDX_e12875dfb3b1d92d7d7c5377e2\` (\`email\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
-        await queryRunner.query(`CREATE TABLE \`post\` (\`id\` varchar(36) NOT NULL, \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`title\` varchar(255) NOT NULL, \`titleSub\` varchar(255) NOT NULL, \`content\` varchar(255) NOT NULL, \`userId\` varchar(36) NULL, INDEX \`IDX_fb91bea2d37140a877b775e6b2\` (\`createdAt\`), INDEX \`IDX_5c1cf55c308037b5aca1038a13\` (\`userId\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
+        await queryRunner.query(`CREATE TABLE \`post\` (\`id\` varchar(36) NOT NULL, \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`title\` varchar(255) NOT NULL, \`titleSub\` varchar(255) NOT NULL, \`content\` varchar(255) NOT NULL, \`userId\` varchar(36) NULL, INDEX \`IDX_fb91bea2d37140a877b775e6b2\` (\`createdAt\`), INDEX \`IDX_68fcfda5aeecc854b4e1f9064c\` (\`id\`, \`userId\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`ALTER TABLE \`tag\` ADD CONSTRAINT \`FK_7435e891c35f2687d7969490476\` FOREIGN KEY (\`postId\`) REFERENCES \`post\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE \`user_session\` ADD CONSTRAINT \`FK_b5eb7aa08382591e7c2d1244fe5\` FOREIGN KEY (\`userId\`) REFERENCES \`user\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE \`post\` ADD CONSTRAINT \`FK_5c1cf55c308037b5aca1038a131\` FOREIGN KEY (\`userId\`) REFERENCES \`user\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
@@ -19,7 +19,7 @@ export class init1642101645315 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE \`post\` DROP FOREIGN KEY \`FK_5c1cf55c308037b5aca1038a131\``);
         await queryRunner.query(`ALTER TABLE \`user_session\` DROP FOREIGN KEY \`FK_b5eb7aa08382591e7c2d1244fe5\``);
         await queryRunner.query(`ALTER TABLE \`tag\` DROP FOREIGN KEY \`FK_7435e891c35f2687d7969490476\``);
-        await queryRunner.query(`DROP INDEX \`IDX_5c1cf55c308037b5aca1038a13\` ON \`post\``);
+        await queryRunner.query(`DROP INDEX \`IDX_68fcfda5aeecc854b4e1f9064c\` ON \`post\``);
         await queryRunner.query(`DROP INDEX \`IDX_fb91bea2d37140a877b775e6b2\` ON \`post\``);
         await queryRunner.query(`DROP TABLE \`post\``);
         await queryRunner.query(`DROP INDEX \`IDX_e12875dfb3b1d92d7d7c5377e2\` ON \`user\``);
