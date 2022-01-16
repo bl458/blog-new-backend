94ed6fcf5825164fd0d12ced54c1a18e7897b50d
Byungchan Lim <bl458@cornell.edu>
2022-01-16 14:40:06 +0900

    FEAT create emailPipe

---
+++ src/pipes/EmailPipe.ts
@@ -0,0 +1,25 @@
+import {
+  PipeTransform,
+  Injectable,
+  ArgumentMetadata,
+  BadRequestException,
+} from '@nestjs/common';
+
+@Injectable()
+export class EmailPipe implements PipeTransform {
+  transform(val: any, { metatype, data }: ArgumentMetadata) {
+    if (val === undefined) {
+      throw new BadRequestException(`${data} must be defined`);
+    }
+
+    if (metatype !== String) {
+      throw new BadRequestException(`${data} has wrong type. Got ${metatype}`);
+    }
+
+    if (typeof val !== 'string') {
+      throw new BadRequestException(
+        `${data} must be a string. Got ${typeof val}`,
+      );
+    }
+  }
+}
